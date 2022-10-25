ff2deadbf0d56f027c6d01591afaa69102cace2a
Vedasto <prajesh.eleven118@gmail.com>
2022-10-25 23:13:11 +0530

    added navigation packages

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-red-500" >
-        Hey!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-red-500" >
+          Hey!
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
