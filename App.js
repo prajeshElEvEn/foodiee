8b855645ec571b0ab737b8176db74619a1f2b541
Vedasto <prajesh.eleven118@gmail.com>
2022-10-25 23:06:29 +0530

    - fixed issue with tailwind integration

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text className="text-red-500">Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-500" >
+        Hey!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
