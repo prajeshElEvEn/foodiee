6662955c93c9d53515fd441c64400de638109fff
Vedasto <prajesh.eleven118@gmail.com>
2022-10-25 22:55:26 +0530

    modified files

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className="text-red-500">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
