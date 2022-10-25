0a67ac31f6a436ca99286b10922926f0219eaf0a
Vedasto <prajesh.eleven118@gmail.com>
2022-10-25 23:17:31 +0530

    updated app.js

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
@@ -12,5 +15,5 @@ export default function App() {
         <StatusBar style="auto" />
       </View>
     </NavigationContainer>
-  );
+  )
 }
